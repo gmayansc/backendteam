f18df079aff0ab3784a047958eb4912849475cfe
Elvira Diz <ediz@uoc.edu>
2022-04-27 00:13:47 +0200

    apartado 6c

--- src/main/java/edu/uoc/backendteam/paracasa/controller/ClienteController.java
+++ src/main/java/edu/uoc/backendteam/paracasa/controller/ClienteController.java
@@ -5,29 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import edu.uoc.backendteam.paracasa.dao.ClienteRepository;
-import edu.uoc.backendteam.paracasa.dao.ProductoRepository;
-import edu.uoc.backendteam.paracasa.model.Producto;
-
-import javax.validation.Valid;
+import edu.uoc.backendteam.paracasa.dao.UsuarioRepository;
+import edu.uoc.backendteam.paracasa.model.Usuario;
 
 @Controller
 public class ClienteController {
 	@Autowired
-	private ClienteRepository clienteRepository;
+	private UsuarioRepository usuarioRepository;
 	
 	
     @GetMapping("/clientes")
     public String clientes(Model model) {
+    	
+    	Iterable<Usuario> lstUsuario = usuarioRepository.findAll();
 
-        clienteRepository.findAll().forEach(p-> System.out.println(p.getNombre()));
-        model.addAttribute("datos", clienteRepository.findAll());
+    	lstUsuario.forEach(p-> System.out.println(p.getUsername()));
+        model.addAttribute("datos", lstUsuario);
         return "index-cliente";
     }
 
